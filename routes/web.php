974cc4c6f27653be69cb5b145f5c936bf11d94e7
Nubia Cantú <2030210@upv.edu.mx>
2023-10-05 17:31:26 -0600

    arduino

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\MaestrosController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArduinoController;
 
 /*
 |--------------------------------------------------------------------------
